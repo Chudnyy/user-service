05ce87e9a5702c5b7a35fcc9a156cccbf1555444
Chudnyi <chudnyy@ukr.net>
2026-07-24 11:40:20 +0200

    [POSTGRES]: switch controller from userInMemoryService to UserService. remove UserInMemoryService

--- src/main/kotlin/pet/integration/users/controller/UserController.kt
+++ src/main/kotlin/pet/integration/users/controller/UserController.kt
@@ -1,20 +1,20 @@
 package pet.integration.users.controller
 
 import pet.integration.users.model.UserDto
-import pet.integration.users.service.UserInMemoryService
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pet.integration.users.service.UserService
 
 @RestController
 @RequestMapping("$BASE_PATH/users")
 internal class UserController(
-    private val userInMemoryService: UserInMemoryService,
+    private val userService: UserService,
 ) {
 
     @GetMapping("/all")
     @Operation(summary = "Get all users")
-    fun getUsers(): List<UserDto> = userInMemoryService.getAll()
+    fun getUsers(): List<UserDto> = userService.getAll()
 
 }
--- src/test/kotlin/pet/integration/users/controller/UserControllerTest.kt
+++ src/test/kotlin/pet/integration/users/controller/UserControllerTest.kt
@@ -2,20 +2,35 @@ package pet.integration.users.controller
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.client.RestTestClient
+import pet.integration.users.AbstractBootTest
 import pet.integration.users.model.UserDto
+import pet.integration.users.service.UserService
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class UserControllerTest(
     @param:Autowired private val userController: UserController,
-) {
+) : AbstractBootTest() {
+
+    @MockitoBean
+    private lateinit var userService: UserService
 
     private val client = RestTestClient.bindToController(userController).build()
 
     @Test
     fun `should return all users`() {
+
+        // Given
+        val userList = listOf(
+            UserDto("John Doe", "a@a.a"),
+            UserDto("Jane Doe", "b@b.b"),
+        )
+        whenever(userService.getAll()).thenReturn(
+            userList
+        )
+
         // when
         val actual = client
             .get()
@@ -26,12 +41,7 @@ internal class UserControllerTest(
             .responseBody
 
         // then
-        assertThat(actual).hasSize(2).containsExactlyInAnyOrderElementsOf(
-            listOf(
-                UserDto("John Dou", "a@a.a"),
-                UserDto("Jane Doe", "b@b.b"),
-            )
-        )
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(userList)
     }
 
 }
